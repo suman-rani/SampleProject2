e95a9bdad997b231c33c0a60e034364b9318baa8
suman-rani <sumanranij2014@gmail.com>
2020-07-26 12:44:53 +0530

    SampleProject2

---
+++ VarEx.java
@@ -0,0 +1,14 @@
+package com.OOPs;
+
+public class VarEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int m1=90,m2=77,m3=69;
+        System.out.println("Subject Marks\nMaths "+m1 +"\ncomputer " +m2 +" \nchemistry "+m3);
+	    int result;
+	    result=m1+m2;
+	    System.out.println("Sum ="+result);
+	}
+
+}
